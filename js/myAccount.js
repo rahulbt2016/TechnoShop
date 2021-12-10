b9abb88e54f6beb10042e881441cae67141b28bc
Rahul Tiwari <rahulbt2016@gmail.com>
2021-12-10 01:59:23 -0500

    Update myAccount.js

--- js/myAccount.js
+++ js/myAccount.js
@@ -4,6 +4,59 @@ if(localStorage.getItem("loggedInUser") === null || localStorage.loggedInUser ==
     localStorage.setItem("loggedInUser", "rahulbt2016@gmail.com");
 }
 
+//Creating users if doesn't already exist
+if(localStorage.getItem("users") === null || localStorage.users == ""){
+    
+    //Creating an array of objects (users)
+    let users = [
+        {
+            name: "Rahul Tiwari",
+            email: "rahulbt2016@gmail.com",
+            password: "abc#1234",
+            phone: "4379917639",
+            address: "1530 Albion Road,\nEtobicoke,\nToronto ON,\nM9V-2J1",
+            image: "images/default-user-image.png"
+        },
+        {
+            name: "Mansi Patel",
+            email: "mansipatel@gmail.com",
+            password: "abc#1234",
+            phone: "454448785",
+            address: "15 Albion Road,\nEtobicoke,\nToronto ON,\nM9V-2J1",
+            image: "images/default-user-image.png"
+        },
+        {
+            name: "Rohan Patel",
+            email: "rohanpatel@gmail.com",
+            password: "abc#1234",
+            phone: "4343455688",
+            address: "1530 Albion Road,\nEtobicoke,\nToronto ON,\nM9V-2J1",
+            image: "images/default-user-image.png"
+        },
+        {
+            name: "Raina Patel",
+            email: "rainapatel@gmail.com",
+            password: "abc#1234",
+            phone: "4333455687",
+            address: "1530 Albion Road,\nEtobicoke,\nToronto ON,\nM9V-2J1",
+            image: "images/default-user-image.png"
+        },
+        {
+            name: "Raveena Katariya",
+            email: "raveenak@gmail.com",
+            password: "abc#1234",
+            phone: "4388755555",
+            address: "1530 Albion Road,\nEtobicoke,\nToronto ON,\nM9V-2J1",
+            image: "images/default-user-image.png"
+        }
+    ];
+
+    //converting the array of objects into string and storing it in the local storage
+    if(localStorage.getItem("users") === null)
+        localStorage.setItem("users", JSON.stringify(users));
+
+}
+
 //Execute when page is fully loaded
 $(document).ready(function(){
     
